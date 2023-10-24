74232a3c4378c50b2851de3dd8d4d76ddf72700c
azazaikens <rubanjuri@gmail.com>
2023-10-24 22:50:20 +0300

    remove

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,26 +9,7 @@ export const App = () => {
   const [contacts, setContacts] = useState(
     () => JSON.parse(localStorage.getItem('key')) ?? []
   );
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
 
-  // componentDidMount() {
-  //   const contacts = JSON.parse(localStorage.getItem('contacts'));
-  //   if (contacts) {
-  //     this.setState({contacts})
-  //   }
-  // }
-
-  // componentDidUpdate() {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // };
   useEffect(() => {
     const stringifiedContacts = JSON.stringify(contacts);
     localStorage.setItem('key', stringifiedContacts);
